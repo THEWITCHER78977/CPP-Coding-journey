9d3bd99e6a532f2bfb20ba7d263994000fdc12f6
Atlas <ahmed.abdelli6576@gmail.com>
2026-07-07 01:31:14 +0100

    Add files via upload

---
+++ Level#2/Problem/Problem#27.cpp
@@ -0,0 +1,67 @@
+// Problem#26
+#include <iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+	//Function to generate a random number
+
+	int randNum = rand() % (To - From + 1) + From;
+	return randNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+	cout << "\nenter number Of Elements: ";
+	cin >> arrLength;
+
+	for (int i = 0; i < arrLength; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n";
+}
+
+int SumOfArray(int arr[100], int arrLength)
+{
+	int Sum = 0;
+	for (int i = 0; i < arrLength; i++)
+	{
+		Sum = Sum + arr[i];
+	}
+	return Sum;
+}
+
+float ArrayAverage(int arr[100], int arrLength)
+{
+	return (float)SumOfArray(arr, arrLength) / arrLength;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int arr[100], arrLength;
+
+	FillArrayWithRandomNumbers(arr, arrLength);
+	
+
+	cout << "\nArray  Elements: ";
+	PrintArray(arr, arrLength);
+
+	cout << "\nAverage of Array is : ";
+	cout << ArrayAverage(arr, arrLength);
+
+
+	return 0;
+}
