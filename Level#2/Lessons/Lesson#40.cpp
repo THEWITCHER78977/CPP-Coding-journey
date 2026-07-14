ed9058ba384d189fd6121484ff16e100fee4efd1
Atlas <ahmed.abdelli6576@gmail.com>
2026-07-14 03:40:59 +0100

    Add files via upload

---
+++ Level#2/Lessons/Lesson#40.cpp
@@ -0,0 +1,39 @@
+// Lesson#40 -  Pointers and Arrays
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[4] = { 10,20,30,40 };
+	int* ptr;
+	ptr = arr;
+
+	//ptr is equivalent to &arr[0];
+	//ptr + 1 is equivalent to &arr[1];
+	//ptr + 2 is equivalent to &arr[2];
+	//ptr + 3 is equivalent to &arr[3];
+
+	cout << "Addresses of array are : \n";
+
+	cout << ptr << endl;
+	cout << ptr + 1 << endl;
+	cout << ptr + 2 << endl;
+	cout << ptr + 3 << endl;
+
+	cout << "\nValues of array are : \n";
+	cout << *(ptr) << endl;
+	cout << *(ptr + 1) << endl;
+	cout << *(ptr + 2) << endl;
+	cout << *(ptr + 3) << endl;
+
+	cout << endl;
+
+	for (int i = 0; i <= 3; i++)
+	{
+		cout << *(ptr + i) << endl;
+	}
+
+
+	return 0;
+}
