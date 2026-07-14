23d8882deee973ede19ba0b6209f4f838550e762
Atlas <ahmed.abdelli6576@gmail.com>
2026-07-14 03:38:25 +0100

    Add files via upload

---
+++ Level#2/Lessons/Lesson#34.cpp
@@ -0,0 +1,20 @@
+// Lesson#34 - Creating References
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int& x = a;
+
+	cout << &a << endl;
+	cout << &x << endl;
+
+	x = 20;
+
+	cout << a << endl;
+	cout << x << endl;
+
+	return 0;
+}
